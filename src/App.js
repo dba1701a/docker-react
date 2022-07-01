fec353d6210ecca0f5b6fd3757c4392ebec5fc86
dba1701a <dba1701a@yahoo.com>
2022-07-01 11:41:24 -0400

    AWS test02

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Compose tested by DMT says .. Edit <code>src/App.js</code> and save to reload.
+          Configure on AWS by DMT says .. Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
